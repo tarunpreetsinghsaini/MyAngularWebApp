019fd1b81edef5ac4ae34d0417f58d75e17f3731
tarunpreetsingh <tarunpreetsinghsaini99@gmail.com>
2020-05-12 00:44:19 +0530

    This is my first module

---
+++ app.js
@@ -0,0 +1,42 @@
+(function () {
+	'use strict';
+
+
+angular.module('MyFirstApp', [])
+
+.controller('MyFirstController', function ($scope) {
+
+	$scope.inputeat = "";
+	$scope.Output  = "";
+	$scope.Checker = function (){
+				
+				if($scope.inputeat != ""){
+
+					$scope.Output = GetUserMsg( GetLunchItems($scope.inputeat) );
+
+				}
+				else{
+					$scope.Output =  "Please enter data first";
+				}
+	};
+
+	function GetLunchItems(items){
+		var i = items.split(',');
+		return i.length;
+	}
+
+	function GetUserMsg(input){
+		if(input <= 3){
+			return "Enjoy!";
+		}
+		else{
+			return "Too much!";
+		}
+
+	}
+
+});
+
+
+
+})();
